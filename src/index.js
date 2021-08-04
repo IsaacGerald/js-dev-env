62c1b2d76fcabdbdf9f7bbb193f33cbe3fda45a3
IsaaqGerald <geraldodhiambo47@gmail.com>
2021-08-04 15:01:12 +0300

    mock hhtp call

--- src/index.js
+++ src/index.js
@@ -1,5 +1,33 @@
+import { getUsers, deleteUser } from './api/userApi';
 import './index.css';
-import numeral from 'numeral';
 
-const courseValue = numeral(1000).format('$0,00.00');
-console.log(`I would pay ${courseValue} for this awesome course`);//eslint-disable-line no-console
+//Populate table of users via Api call
+getUsers().then(result => {
+  let usersBody = "";
+
+  result.forEach(user => {
+    usersBody += `<tr>
+    <td><a href="#" data-id="${user.id}" class="deleteUser">Delete</td>
+    <td>${user.id}</td>
+    <td>${user.firstName}</td>
+    <td>${user.lastName}</td>
+    <td>${user.email}</td>
+    </tr>`
+  });
+
+  global.document.getElementById('users').innerHTML = usersBody;
+
+  const deleteLinks = global.document.getElementsByClassName('deleteUser');
+
+  // Must use array.from to create a real array from a DOM collection
+  // getElementsByClassname only returns an "array like" object
+  Array.from(deleteLinks, link => {
+    link.onclick = function (event) {
+      const element = event.target;
+      event.preventDefault();
+      deleteUser(element.attributes["data-id"].value);
+      const row = element.parentNode.parentNode;
+      row.parentNode.removeChild(row);
+    };
+  });
+});
--- src/index.test.js
+++ src/index.test.js
@@ -9,12 +9,12 @@ describe('Our first test', () => {
 });
 
 describe('index.html', () => {
-  it('Should say hello', () => {
+  it('Should have h1 as users', () => {
     const index = fs.readFileSync('./src/index.html', 'utf-8');
     const {JSDOM} = jsdom;
     const dom = new JSDOM(index);
     const h1 = dom.window.document.getElementsByTagName("h1")[0];
-    expect(h1.innerHTML).to.equal("Hello world!");
+    expect(h1.innerHTML).to.equal("Users");
     dom.window.close();
   } )
 });
